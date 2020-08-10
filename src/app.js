6bbd345b2d6d0d0003c18d3d6de83d8e849d740b
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2020-08-09 21:14:09 -0400

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const static = express.static(__dirname + "/public");
-const configRoutes = require("./routes");
-const exphbs = require("express-handlebars");
-const Handlebars = require("handlebars");
+const session = require('express-session');
+const static = express.static(__dirname + '/public');
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 
-app.use("/public", static);
+app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine(
-  "handlebars",
-  exphbs({ layoutsDir: "src/views/layouts", defaultLayout: "main" })
+app.engine('handlebars', exphbs({ layoutsDir: 'src/views/layouts', defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(
+    session({
+        name: 'AuthCookie', //name the cookie, idk what to call it...
+        secret: 'Not Gyro the food...Gyro the Jira knockoff',
+        resave: false,
+        saveUninitialized: true,
+    })
 );
-app.set("view engine", "handlebars");
+
+//middleware goes here
 
 configRoutes(app);
 
 app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log("Your routes will be running on http://localhost:3000");
+    console.log("We've now got a server!");
+    console.log('Your routes will be running on http://localhost:3000');
 });
