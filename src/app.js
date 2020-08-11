2f9cb45abc4b27ce33aa954480ddfbd90e335c3c
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2020-08-11 15:27:30 -0400

    added middleware func for rerouting user if not logged in

--- src/app.js
+++ src/app.js
@@ -10,23 +10,37 @@ app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', exphbs({ layoutsDir: 'src/views/layouts', defaultLayout: 'main' }));
+app.engine(
+	'handlebars',
+	exphbs({ layoutsDir: 'src/views/layouts', defaultLayout: 'main' })
+);
 app.set('view engine', 'handlebars');
 
 app.use(
-    session({
-        name: 'AuthCookie', //name the cookie, idk what to call it...
-        secret: 'Not Gyro the food...Gyro the Jira knockoff',
-        resave: false,
-        saveUninitialized: true,
-    })
+	session({
+		name: 'AuthCookie', //name the cookie, idk what to call it...
+		secret: 'Not Gyro the food...Gyro the Jira knockoff',
+		resave: false,
+		saveUninitialized: true
+	})
 );
 
-//middleware goes here
+/*
+middlware function for rerouting to the login page if user isn't logged in
+*/
+app.use(async (req, res, next) => {
+	if (!req.session.user) {
+		return res
+			.status(403)
+			.render('../src/views/login/index', { error: true });
+	} else {
+		next();
+	}
+});
 
 configRoutes(app);
 
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3000');
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
 });
