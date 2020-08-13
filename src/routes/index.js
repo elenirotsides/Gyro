0a4c667eaa300d46ec76ae0bc5b7c928992cf95c
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2020-08-13 09:11:54 -0400

    removed some stuff from index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,24 +2,12 @@ const loginRoutes = require('./login');
 const boardRoutes = require('./board');
 const registerRoutes = require('./register');
 const tasksRoutes = require('./tasks');
-//const session = require("express-session");
-
-//I added session stuff in app.js, we can change this later if we want it here but I think
-//it be best that we keep all middleware in app.js since that's where we configure stuff ~Eleni
 
 const constructorMethod = (app) => {
 	app.use('/login', loginRoutes);
 	app.use('/board', boardRoutes);
 	app.use('/register', registerRoutes);
 	app.use('/tasks', tasksRoutes);
-	/*
-	app.get('/', (req, res) => {
-		if (!req.session.user) {
-			res.redirect('/login');
-		} else {
-			res.redirect('/board');
-		}
-	}); */
 
 	app.get('/logout', async (req, res) => {
 		req.session.destroy();
