67bd58acc03de241c87b6670dab8438ce7bc44bf
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2020-08-13 23:12:43 -0400

    fully functioning POST /register route

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,77 +1,102 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../datalayer/users');
-const connection = require('../datalayer/mongoConnection');
 
 router.get('/', async (req, res) => {
-	res.render('../src/views/login/register');
+	res.render('../src/views/login/register', {
+		title: 'Register',
+		hideLogout: true
+	});
 });
 
 router.post('/', async (req, res) => {
 	input = req.body;
 
-	//add user to users table
-	//log user in
-	//route to kanban board
+	/* 
+	I made all the fields required in the html using the 'required' keyword.
+	The below error handling takes care of the case when a user inputs only spaces
+	*/
 
-	//error handling below, but I made the html fields required, so it may never even hit this point
-	//thinking of checking for blank input, test that Eleni
-	if (!input['firstName']) {
-		return res.status(400).render('../src/views/login/register', {
-			hasErrors: true,
-			error: 'Uh oh! Something went wrong. Please try again.'
-		});
+	if (!input['firstName'].trim()) {
+		return res
+			.status(400)
+			.render('../src/views/login/register', {
+				title: 'Error',
+				hideLogout: true,
+				hasErrors: true,
+				error: 'Uh oh! No blank inputs allowed, please try again.'
+			});
 	}
-	if (!input['lastName']) {
-		return res.status(400).render('../src/views/login/register', {
-			hasErrors: true,
-			error: 'Uh oh! Something went wrong. Please try again.'
-		});
+	if (!input['lastName'].trim()) {
+		return res
+			.status(400)
+			.render('../src/views/login/register', {
+				title: 'Error',
+				hideLogout: true,
+				hasErrors: true,
+				error: 'Uh oh! No blank inputs allowed, please try again.'
+			});
 	}
-	if (!input['email']) {
-		return res.status(400).render('../src/views/login/register', {
-			hasErrors: true,
-			error: 'Uh oh! Something went wrong. Please try again.'
-		});
+	if (!input['email'].trim()) {
+		return res
+			.status(400)
+			.render('../src/views/login/register', {
+				title: 'Error',
+				hideLogout: true,
+				hasErrors: true,
+				error: 'Uh oh! No blank inputs allowed, please try again.'
+			});
 	}
-	if (!input['password']) {
-		return res.status(400).render('../src/views/login/register', {
-			hasErrors: true,
-			error: 'Uh oh! Something went wrong. Please try again.'
-		});
+	if (!input['password'].trim()) {
+		return res
+			.status(400)
+			.render('../src/views/login/register', {
+				title: 'Error',
+				hideLogout: true,
+				hasErrors: true,
+				error: 'Uh oh! No blank inputs allowed, please try again.'
+			});
+	}
+
+	let emailExists;
+
+	try {
+		emailExists = await users.getUserByEmail(input['email'].toLowerCase());
+	} catch {
+		emailExists = false;
 	}
 
 	let addUser;
-	//add logic here for not being able to add a user if the email already exists
-	try {
-		addUser = await users.addUser(
-			input['firstName'],
-			input['lastName'],
-			input['email'].toLowerCase(),
-			input['password']
-		);
-	} catch (e) {
-		return res.status(400).render('../src/views/login/register', {
-			hasErrors: true,
-			error: 'Whoops! Something has gone wrong, please try again.'
-		}); //fix this
-	}
 
-	let dbUserEmail = await users.getUserByEmail(addUser.email);
-	if (dbUserEmail === input['email'].toLowerCase()) {
-		//checking to make sure that the user was inserted into the db
+	if (emailExists === false) {
+		try {
+			addUser = await users.addUser(
+				input['firstName'],
+				input['lastName'],
+				input['email'].toLowerCase(),
+				input['password']
+			);
+		} catch (e) {
+			res.status(400).render('../src/views/login/register', {
+				title: 'Error',
+				hideLogout: true,
+				hasErrors: true,
+				error: 'Whoops! Something has gone wrong, please try again.'
+			});
+		}
 	} else {
 		return res.status(400).render('../src/views/login/register', {
+			title: 'Error',
+			hideLogout: true,
 			hasErrors: true,
-			error: 'Error registering, please try again'
+			error:
+				'Sorry, account already exists with that email. Please try registering again.'
 		});
 	}
-	//if addUser was successful, set req.session.user and route to /board
-	//add a middleware to only show /login page to non-authenticated users
-	res.redirect('/login'); //fix this
 
-	const db = await connection();
-	await db.serverConfig.close();
+	//if addUser was successful, req.session.user will be set and user will be redirected to /board
+	req.session.user = addUser;
+	res.redirect('/board');
 });
 
 module.exports = router;
